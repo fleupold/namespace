b738657c66dbf891c25d3931f6a6a7a4e5adcb84
Felix Leupold <felixleupold90@gmail.com>
2019-03-13 14:38:15 +0100

    first commit

---
+++ app.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"encoding/json"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/fleupold/nameservice/x/nameservice"
+
+	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+const (
+    appName = "nameservice"
+)
+
+type nameServiceApp struct {
+    *bam.BaseApp
+}
+
+func NewNameServiceApp(logger log.Logger, db dbm.DB) *nameServiceApp {
+
+    // First define the top level codec that will be shared by the different modules. Note: Codec will be explained later
+    cdc := MakeCodec()
+
+    // BaseApp handles interactions with Tendermint through the ABCI protocol
+    bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
+
+    var app = &nameServiceApp{
+        BaseApp: bApp,
+        cdc:     cdc,
+    }
+
+    return app
+}
